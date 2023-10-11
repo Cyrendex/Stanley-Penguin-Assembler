64556e1dc74b386b5d3a992a10cf41bcea4f8fd2
Eren Unsal <aaronunsal@gmail.com>
2023-10-11 00:40:56 -0700

    Created src folder including spasm.py

---
+++ src/spasm.py
@@ -0,0 +1,21 @@
+"""
+spasm.py - Stanley/Penguin Processor Assembler
+
+Description:
+    This Python script serves as an assembler for the Stanley/Penguin processor, converting assembly code
+    into machine code that can be executed on the corresponding hardware.
+
+Usage:
+    NOT YET IMPLEMENTED
+
+Version:
+    1.0
+
+For more information, refer to the README.md file or visit https://github.com/Cyrendex/Stanley-Penguin-Assembler.
+"""
+
+
+
+
+
+
